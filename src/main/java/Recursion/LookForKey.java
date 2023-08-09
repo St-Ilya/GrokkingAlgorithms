a97887eadb37d0750a360bbfff747860ebd455af
Karpuhin Ilya <karpuhinilya@gmail.com>
2023-08-09 19:12:01 +0300

    recursion deff LookForKey.java

---
+++ src/main/java/Recursion/LookForKey.java
@@ -0,0 +1,17 @@
+package Recursion;
+
+public class LookForKey {
+    public static void main(String[] args) {
+
+    }
+
+    public static void lookForKey(Box box) {
+        for (Item item : box) {
+            if (item.isABox()) {
+                lookForKey(item);
+            } else if (item.isAKey()) {
+                System.out.println("found the key!");
+            }
+        }
+    }
+}
