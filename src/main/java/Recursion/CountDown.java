14b90d2fc744cc1c57d93aed6bc530e17688acd9
Karpuhin Ilya <karpuhinilya@gmail.com>
2023-08-10 15:12:20 +0300

    func countDownBase with if-else

--- src/main/java/Recursion/CountDown.java
+++ src/main/java/Recursion/CountDown.java
@@ -3,12 +3,22 @@ package Recursion;
 public class CountDown {
     public static void main(String[] args) {
         int i = 10;
-        countDown(i);
+        //countDown(i);
+        countDownBase(i);
     }
 
     // infinite func
     public static void countDown (int i) {
         System.out.println(i);
         countDown(i - 1);
+    }
+
+    public static void countDownBase(int i) {
+        System.out.println(i);
+        if (i <= 1) {
+            return;
+        } else {
+            countDownBase(i - 1);
+        }
     }
 }
