6b2e3201de2b288ec7e12f3f2dddd72f6511065e
Karpuhin Ilya <karpuhinilya@gmail.com>
2023-08-12 13:21:37 +0300

    the quick sort algorithm is performed via streams. You will need to parse the streams in return

--- src/main/java/Ch4_QuickSort/Example5_QuickSort/QuickSort.java
+++ src/main/java/Ch4_QuickSort/Example5_QuickSort/QuickSort.java
@@ -1,38 +1,33 @@
 package Ch4_QuickSort.Example5_QuickSort;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class QuickSort {
     public static void main(String[] args) {
-        System.out.println(quickSort(new ArrayList<>(Arrays.asList(10, 5, 2, 3))));
+        System.out.println(quickSort(Arrays.asList(10, 5, 2, 3)));
     }
 
     public static List<Integer> quickSort(List<Integer> array) {
         if (array.size() < 2) {
+            // base case, if there are less than two elements, sorting is not required
             return array;
         } else {
-            int pivot = array.get(0);
-            List<Integer> less = new ArrayList<>();
-            List<Integer> greater = new ArrayList<>();
+            // recursive case
+            Integer pivot = array.get(0);
 
-            for (Integer integer : array) {
-                if (integer <= pivot) {
-                    less.add(integer);
-                } else {
-                    greater.add(integer);
-                }
-            }
-            List<Integer> sortedLess = quickSort(less);
-            List<Integer> sortedGreater = quickSort(greater);
+            // sub-array of all the elements less than the pivot
+            List<Integer> less = array.stream().skip(1).filter(x -> x <= pivot).collect(Collectors.toList());
 
-            List<Integer> sortedArray = new ArrayList<>();
-            sortedArray.addAll(sortedLess);
-            sortedArray.add(pivot);
-            sortedArray.addAll(sortedGreater);
+            // // sub-array of all the elements greater than the pivot
+            List<Integer> greater = array.stream().skip(1).filter(x -> x > pivot).collect(Collectors.toList());
 
-            return sortedArray;
+            return Stream.of(quickSort(less).stream(),
+                    Stream.of(pivot),
+                    quickSort(greater).stream()).flatMap(Function.identity()).collect(Collectors.toList());
         }
 
     }
