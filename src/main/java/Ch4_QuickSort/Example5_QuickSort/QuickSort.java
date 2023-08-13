b1658f9f52a5fc81758becabe343eaafdf5b89bf
Karpuhin Ilya <karpuhinilya@gmail.com>
2023-08-13 15:32:43 +0300

    add my implementation of the quick sort algorithm

--- src/main/java/Ch4_QuickSort/Example5_QuickSort/QuickSort.java
+++ src/main/java/Ch4_QuickSort/Example5_QuickSort/QuickSort.java
@@ -1,5 +1,6 @@
 package Ch4_QuickSort.Example5_QuickSort;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
@@ -8,16 +9,19 @@ import java.util.stream.Stream;
 
 public class QuickSort {
     public static void main(String[] args) {
+        System.out.println(Arrays.toString(Thread.currentThread().getStackTrace()));
         System.out.println(quickSort(Arrays.asList(10, 5, 2, 3)));
+        System.out.println(myQuickSort(Arrays.asList(10, 5, 2, 3)));
     }
 
     public static List<Integer> quickSort(List<Integer> array) {
+        System.out.println(Arrays.toString(Thread.currentThread().getStackTrace()));
         if (array.size() < 2) {
             // base case, if there are less than two elements, sorting is not required
             return array;
         } else {
             // recursive case
-            Integer pivot = array.get(0);
+            Integer pivot = array.get(0); // Опорный элемент
 
             // sub-array of all the elements less than the pivot
             List<Integer> less = array.stream().skip(1).filter(x -> x <= pivot).collect(Collectors.toList());
@@ -30,5 +34,28 @@ public class QuickSort {
                     quickSort(greater).stream()).flatMap(Function.identity()).collect(Collectors.toList());
         }
 
+    }
+
+    public static List<Integer> myQuickSort(List<Integer> list) {
+        System.out.println(Arrays.toString(Thread.currentThread().getStackTrace()));
+        if (list.size() < 2) {
+            return list;
+        } else {
+            List<Integer> less = new ArrayList<>();
+            List<Integer> greater = new ArrayList<>();
+            Integer pivot = list.get(0);
+            for (int i = 1; i < list.size(); i++) {
+                if (list.get(i) <= pivot) {
+                    less.add(list.get(i));
+                } else {
+                    greater.add(list.get(i));
+                }
+            }
+            List<Integer> sortedList = new ArrayList<>();
+            sortedList.addAll(myQuickSort(less));
+            sortedList.add(pivot);
+            sortedList.addAll(myQuickSort(greater));
+            return sortedList;
+        }
     }
 }
